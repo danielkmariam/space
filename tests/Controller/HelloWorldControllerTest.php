95952975f70716290e4e1f85a4fc0a78b2f32dcb
Daniel Kidanemariam <danielkmariam@gmail.com>
2021-06-27 10:46:48 +0100

    add phpunit

---
+++ tests/Controller/HelloWorldControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HelloControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+}
