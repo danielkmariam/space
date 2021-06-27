71211337e81a2b06efa366e11ccc6345f1cb3005
Daniel Kidanemariam <danielkmariam@gmail.com>
2021-06-27 12:03:53 +0100

    Add phpstan and ebnable in actions

--- tests/Controller/HelloControllerTest.php
+++ tests/Controller/HelloControllerTest.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-namespace App\Tests;
+namespace App\Tests\Controller;
 
 use PHPUnit\Framework\TestCase;
 
 class HelloControllerTest extends TestCase
 {
-    public function testIndex()
+    public function testIndex(): void
     {
         $this->assertTrue(true);
     }
